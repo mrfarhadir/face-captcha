6b21012a6a5bd47c067f14f0e9b1fcbb641ca7fb
Farhad Mehryari <info@mrfarhad.ir>
2024-08-18 17:13:17 +0200

    rename ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,40 @@
-import globals from "globals";
+/*import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+*/
 
+//export default [
+//  {
+//    files: ["src/**/*.{js,mjs,cjs,ts}"]
+//  },
+//  { languageOptions: { globals: globals.browser } },
+//  pluginJs.configs.recommended,
+//  ...tseslint.configs.recommended,
+//
+// ];
+//
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
   {
-    files: ["src/**/*.{js,mjs,cjs,ts}"]
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
   },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-
 ];
+
+
--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,5 +1,3 @@
 export function sum(a: number, b: number) {
   return a + b;
 }
-
-
